8014ac59a96785bdd0b83e3f97e0a0d824290fc0
grant <964770081@qq.com>
2021-08-15 08:18:39 +0800

    添加了router

---
+++ src/components/md.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>assad</div>
+
+
+</template>
+
+<script>
+export default {
+  name: "md",
+  data() {
+    return {
+      text: '',
+    };
+  },
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+<!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
+    <md></md>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import  md from  '@/components/md'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+    md
+  }
+}
+</script>
